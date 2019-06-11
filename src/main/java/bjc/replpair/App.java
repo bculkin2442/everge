71c35c0ffba04e7c7932ffa06b9528b2a5efb48d
Benjamin J. Culkin <bjculkin@mix.wvu.edu>
2019-06-11 20:01:53 -0300

    Initial commit

---
+++ src/main/java/bjc/replpair/App.java
@@ -0,0 +1,13 @@
+package bjc.replpair;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
