e491562b9cd364374cefaba62622ed029944843c
Ben Culkin <scorpress@gmail.com>
2022-09-27 19:13:39 -0400

    Minor changes

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+/**
+ * CLI tool for command-line text manipulation.
+ * 
+ * @author bjcul
+ *
+ */
+module everge {
+	exports bjc.everge;
+
+	requires transitive bjc.utils;
+	requires transitive esodata;
+	requires junit;
+}
